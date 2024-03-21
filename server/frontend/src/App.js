cce79a7b9f3bf81fa5d98459367c763723f54fd8
DeanRamsy <deanramsy@gmail.com>
2024-03-20 21:55:08 -0400

    User_Management set up

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register"; // Import the Register component
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} /> {/* Add a route for Register component */}
     </Routes>
   );
 }
